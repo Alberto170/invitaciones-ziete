dc9068c3db2a6d0674840b8883b211d10851a9d6
Alberto Martinez <rynzo@Albertos-MacBook-Air.local>
2024-06-07 19:40:56 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+tailwind css
+npx tailwindcss -i ./css/input.css -o ./css/output.css --watch
---
+++ js/wa.js
@@ -0,0 +1,3 @@
+const message = "Hola mucho gusto, quisiera pedir mas informacion sobre sus invitaciones digitales para mi proximo evento";
+const encodedMessage = encodeURIComponent(message);
+console.log(encodedMessage); // Output: Hello%2C%20I%27d%20like%20to%20know%20more%20about%20your%20services.
